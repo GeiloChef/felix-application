58c2be89b823a1e72d10bc7a152f0227768b160e
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2024-01-20 17:04:32 +0100

    Add PrimeVue and make it compatible with tailwindcss

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,23 @@
 import './assets/main.css';
+import 'primevue/resources/themes/lara-dark-blue/theme.css';
 
 import { createPinia } from 'pinia';
+import Button from 'primevue/button';
+import PrimeVue from 'primevue/config';
+import InputText from 'primevue/inputtext';
 import { createApp } from 'vue';
+
 
 import App from './App.vue';
 import router from './router';
 
 const app = createApp(App);
 
+// eslint-disable-next-line vue/no-reserved-component-names
+app.component('Button', Button);
+app.component('InputText', InputText);
+
+app.use(PrimeVue);
 app.use(createPinia());
 app.use(router);
 
