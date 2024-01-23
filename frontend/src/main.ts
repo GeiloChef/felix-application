838acfc2ce022eb268f0fdbbbe955ad7c3cbc845
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2024-01-23 18:54:35 +0100

    Add default components from primevue and fontawesome

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,11 @@
 import './assets/main.css';
 import 'primevue/resources/themes/lara-dark-blue/theme.css';
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { createPinia } from 'pinia';
 import Button from 'primevue/button';
 import PrimeVue from 'primevue/config';
+import Image from 'primevue/image';
 import InputText from 'primevue/inputtext';
 import { createApp } from 'vue';
 
@@ -14,9 +16,16 @@ import { i18n } from '@/i18n/config';
 
 const app = createApp(App);
 
+/* add font awesome icon component */
+app.component('font-awesome-icon', FontAwesomeIcon);
 // eslint-disable-next-line vue/no-reserved-component-names,vue/multi-word-component-names
 app.component('Button', Button);
+// eslint-disable-next-line vue/no-reserved-component-names
+app.component('Image', Image);
 app.component('InputText', InputText);
+
+/* add all icon we need to the font-awesome library */
+import '/src/icons/fontAwesomeIconsConfig';
 
 app.use(PrimeVue);
 app.use(i18n);
