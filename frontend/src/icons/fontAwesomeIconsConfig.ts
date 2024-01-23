f4763a5d37aafa8d6f99c297f3cf454e9b9e80ea
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2024-01-23 18:54:02 +0100

    Add icons for personal information

--- frontend/src/icons/fontAwesomeIconsConfig.ts
+++ frontend/src/icons/fontAwesomeIconsConfig.ts
@@ -1,8 +1,21 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {
+  faVuejs,
+} from '@fortawesome/free-brands-svg-icons';
+import {
   faUserSecret,
+  faCalendarDays,
+  faLaptopCode,
+  faBusinessTime,
+  faMagnifyingGlass
 } from '@fortawesome/free-solid-svg-icons';
+
 
 library.add(
   faUserSecret,
+  faCalendarDays,
+  faLaptopCode,
+  faBusinessTime,
+  faVuejs,
+  faMagnifyingGlass,
 );
