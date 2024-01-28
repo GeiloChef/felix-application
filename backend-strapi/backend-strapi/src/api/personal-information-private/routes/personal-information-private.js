97f7e25cbc60375115b5fcbecdf09b4dbd69ef45
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2024-01-28 13:15:50 +0100

    Add private personal information to strapi

---
+++ backend-strapi/backend-strapi/src/api/personal-information-private/routes/personal-information-private.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * personal-information-private router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::personal-information-private.personal-information-private');
---
+++ backend-strapi/backend-strapi/src/api/personal-information-private/services/personal-information-private.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * personal-information-private service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::personal-information-private.personal-information-private');
