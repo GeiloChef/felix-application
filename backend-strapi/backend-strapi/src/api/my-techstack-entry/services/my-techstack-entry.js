33c41be89ea38d40459e08255dc96fc6e6ae140a
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2024-01-28 13:15:32 +0100

    Add tech stack to strapi

---
+++ backend-strapi/backend-strapi/src/api/my-techstack-entry/services/my-techstack-entry.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * my-techstack-entry service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::my-techstack-entry.my-techstack-entry');
