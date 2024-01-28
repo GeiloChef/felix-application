94dfefc15fbc37ae8da0cdbed190e933cecec3e3
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2024-01-28 13:02:42 +0100

    Add tech stack to data store

--- frontend/src/stores/dataStore.ts
+++ frontend/src/stores/dataStore.ts
@@ -1,9 +1,10 @@
 import { defineStore } from 'pinia';
-import { ref, computed, type Ref } from 'vue';
+import { ref, type Ref } from 'vue';
 
-import type { Milestone, PersonalInformation } from '@/models/ui-models';
+import type { Milestone, PersonalInformation, TechStackEntry } from '@/models/ui-models';
 import { fetchMilestones } from '@/services/MilestoneService';
 import { fetchPersonalInformation } from '@/services/PersonalInformationService';
+import { fetchTechStack } from '@/services/TechStackService';
 
 /**
  * @description Stores all the data that is coming from the strapi backend that will NOT be changed
@@ -12,6 +13,7 @@ import { fetchPersonalInformation } from '@/services/PersonalInformationService'
 export const useDataStore = defineStore('dataStore', () => {
   const personalInformation: Ref<PersonalInformation> = ref({} as PersonalInformation);
   const milestones: Ref<Milestone[]> = ref([]);
+  const techStack: Ref<TechStackEntry[]> = ref([]);
 
   const getPersonalInformationFromService = async () => {
     fetchPersonalInformation().then((data: PersonalInformation) => personalInformation.value = data);
@@ -23,14 +25,22 @@ export const useDataStore = defineStore('dataStore', () => {
     });
   };
 
+  const getTechStackFromService = async () => {
+    fetchTechStack().then((data: TechStackEntry[]) => {
+      techStack.value = data;
+    });
+  };
+
   const fetchAllInformationForHomeView = async () => {
     getPersonalInformationFromService();
     getMilestonesFromService();
+    getTechStackFromService();
   };
 
   return {
     personalInformation,
     milestones,
+    techStack,
     fetchAllInformationForHomeView
   };
 });
