3747dda5ee20bf78a65f729dee9f7d76621eae89
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2024-01-28 19:46:37 +0100

    Improve BE directory structure [2]

--- backend-strapi/backend-strapi/src/api/external-link/controllers/external-link.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * external-link controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::external-link.external-link');
--- backend-strapi/backend-strapi/src/api/external-link/routes/external-link.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * external-link router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::external-link.external-link');
