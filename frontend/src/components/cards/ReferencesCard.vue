2d3e4601c9d14baf4de752fb89cfbd35ad6df571
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2024-01-29 12:34:45 +0100

    Add references card

---
+++ frontend/src/components/cards/ReferencesCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <ColoredBackgroundCard
+    :title="$t('my-references')"
+    :color-type="ColoredBackgroundCardTypes.Dark">
+    <ReferencesSlider class="mt-12" />
+  </ColoredBackgroundCard>
+</template>
+
+<script setup lang="ts">
+
+  import ColoredBackgroundCard from '@/components/partials/ColoredBackgroundCard.vue';
+  import ReferencesSlider from '@/components/partials/ReferencesSlider.vue';
+  import { ColoredBackgroundCardTypes } from '@/models/core';
+</script>
+
+<style>
+
+</style>
