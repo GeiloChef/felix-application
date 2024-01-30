5d07b2507dbd23fe102b55de47557a4999db50b5
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2024-01-30 11:29:22 +0100

    Setup i18n for localization

--- frontend/src/i18n/config.ts
+++ frontend/src/i18n/config.ts
@@ -3,12 +3,14 @@ import { createI18n } from 'vue-i18n';
 
 import deutsch from './de.json';
 import english from './en.json';
+import spanish from './es.json';
 
 import { Languages } from '@/models/core';
 
 const messages = {
   en: english,
-  de: deutsch
+  de: deutsch,
+  es: spanish
 };
 
 export const i18n = createI18n({
