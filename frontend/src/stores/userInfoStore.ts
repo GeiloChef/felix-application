29808ed11efb552bc5962ced508324738180c665
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2024-01-30 23:45:54 +0100

    Improve user information store to handle authentication and store user data

--- frontend/src/stores/userInfoStore.ts
+++ frontend/src/stores/userInfoStore.ts
@@ -1,10 +1,45 @@
+import { useLocalStorage } from '@vueuse/core';
 import { defineStore } from 'pinia';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
+
+import type { UserData } from '@/models/ui-models';
+import router from '@/router';
+import { postLogin } from '@/services/AuthenticationService';
 
 export const useUserInfoStore = defineStore('userInformation', () => {
-  const isUserLoggedIn = ref(false);
+  const userInfo = useLocalStorage('userInfo', {} as UserData);
+  const jwt = useLocalStorage('jwt', '' as string);
+
+  const isUserLoggedIn = computed((): boolean => {
+    return !!jwt.value;
+  });
+
+  const processLogin = async (username: string, password: string): Promise<boolean> => {
+    const user: false | UserData = await postLogin(username, password);
+
+    if (user) {
+      userInfo.value = user;
+      jwt.value = user.jwt;
+
+      router.push({ name: 'home' });
+
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const processLogout = () => {
+    userInfo.value = {} as UserData;
+    jwt.value = '';
+
+    router.push({ name: 'login' });
+  };
 
   return {
-    isUserLoggedIn
+    userInfo,
+    isUserLoggedIn,
+    processLogin,
+    processLogout
   };
 });
