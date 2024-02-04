891c8f8abf68b5d2e50befa726160adeb362a85a
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2024-02-04 14:28:45 +0100

    Fix axios instance language selection in case the user already has a selected language

--- frontend/src/services/api/axiosInstance.ts
+++ frontend/src/services/api/axiosInstance.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+import type { LocaleEntry } from '@/models/ui-models';
+
 const instance = axios.create({
     baseURL: import.meta.env.VITE_BASE_API_URL
 });
@@ -8,8 +10,17 @@ const instance = axios.create({
  * todo: interceptors
  */
 
+let languageConstant = 'de';
+
+if (localStorage.getItem('selectedLanguage')) {
+  const storedLocaleEntry = JSON.parse(localStorage.getItem('selectedLanguage')!) as LocaleEntry;
+
+  languageConstant = storedLocaleEntry.strapiLocalCode;
+}
+
+
 instance.defaults.params = {
-    'locale': 'de',
+    'locale': languageConstant,
     'populate': '*'
 };
 
