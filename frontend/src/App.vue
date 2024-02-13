595534c0cdfdbcb7d0ada08352c5a19f6b3d1e72
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2024-02-13 21:13:22 +0100

    Add file download dialog

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div :key="selectedLanguage.strapiLocalCode">
+  <div
+    class="relative"
+    :key="selectedLanguage.strapiLocalCode">
     <div v-if="shouldShouldHeaderBar">
       <HeaderBar />
     </div>
@@ -13,11 +15,26 @@
 
     <!--  Guest Disclaimer Dialog  -->
     <GuestDisclaimerDialog />
+
+    <!--  Global Download Dialog  -->
+    <GlobalDownloadDialog />
+
+    <Button
+      class="h-12 w-12 flex flex-row justify-center fixed bottom-16 right-10"
+      size="large"
+      severity="success"
+      @click="fileDownloadStore.toggleFileDownloadDialog">
+      <FontAwesomeIcon
+        icon="file-download"
+        class="text-black" />
+    </Button>
   </div>
 </template>
 
 <script setup lang="ts">
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
   import { storeToRefs } from 'pinia';
+  import Button from 'primevue/button';
   import Toast, { type ToastMessageOptions } from 'primevue/toast';
   import { useToast } from 'primevue/usetoast';
   import { computed, onBeforeMount, onMounted, watch } from 'vue';
@@ -25,10 +42,12 @@
 
   import CookiesDialog from '@/components/partials/CookiesDialog.vue';
   import FooterBar from '@/components/partials/FooterBar.vue';
+  import GlobalDownloadDialog from '@/components/partials/GlobalDownloadDialog.vue';
   import GuestDisclaimerDialog from '@/components/partials/GuestDisclaimerDialog.vue';
   import HeaderBar from '@/components/partials/HeaderBar.vue';
   import { useDataStore } from '@/stores/dataStore';
   import { useFeatureToggleStore } from '@/stores/featureToggleStore';
+  import { useFileDownloadStore } from '@/stores/fileDownloadStore';
   import { useLanguageStore } from '@/stores/languageStore';
   import { useToastMessageStore } from '@/stores/toastMessageStore';
   import { useUserInfoStore } from '@/stores/userInfoStore';
@@ -43,6 +62,7 @@
   const featureToggleStore = useFeatureToggleStore();
 
   const dataStore = useDataStore();
+  const fileDownloadStore = useFileDownloadStore();
 
   const languageStore = useLanguageStore();
   const { selectedLanguage } = storeToRefs(languageStore);
@@ -64,6 +84,7 @@
 
     // asynchronously fetch all data needed for the home view
     dataStore.fetchAllInformationForHomeView();
+    fileDownloadStore.getFileDownloadsFromService();
   });
 
   watch(toastMessages, async (newToastMessages: ToastMessageOptions[]) => {
---
+++ frontend/src/components/partials/GlobalDownloadDialog.vue
@@ -0,0 +1,141 @@
+<template>
+  <Dialog
+    id="GlobalDownloadDialog"
+    v-model:visible="isFileDownloadDialogOpen"
+    modal
+    contentClass="overflow-hidden"
+    :draggable="false"
+    :header="$t('here-you-can-find-all-relevant-documents')"
+    :style="{ width: '80rem' }"
+    :breakpoints="{
+      '640px': '95vw' }">
+    <template #closeicon>
+      <FontAwesomeIcon
+        size="xl"
+        icon="circle-xmark" />
+    </template>
+    <div class="mt-4 mb-10 pl-6 flex flex-row gap-6">
+      <div class="flex flex-col">
+        <span class="p-input-icon-left">
+          <FontAwesomeIcon icon="fa-magnifying-glass" />
+          <InputText
+            id="username"
+            class="w-80 sm:w-96"
+            v-model="searchText"
+            :placeholder="$t('search-for-documents')"/>
+        </span>
+        <small class="mt-1 ml-1 text-gray-400">{{ $t('search-by-name-or-tag') }}</small>
+      </div>
+      <Button
+        v-if="false"
+        class="!max-h-12"
+        :label="$t('filter-documents')"
+        icon="fa-paperclip"
+        size="small">
+      </Button>
+    </div>
+    <DataView
+      class="downloads-data-view-body"
+      :value="filteredDocuments">
+      <template #list="fileDownloadsProps">
+        <div
+          v-for="(fileDownload, index) in fileDownloadsProps.items"
+          :key="index"
+          class="col-12">
+          <div class="flex flex-row gap-4 w-full justify-between items-end px-6">
+            <div class="flex flex-col gap-4">
+              <div class="flex flex-row w-full">
+                <div class="flex flex-col">
+                  <div class="text-2xl font-bold">
+                    {{ fileDownload.headline }}
+                  </div>
+                  <div class="text-md text-gray-400">
+                    {{ fileDownload.subHeadline }}
+                  </div>
+                </div>
+              </div>
+              <div class="flex flex-row gap-2">
+                <Tag
+                  v-for="tag in fileDownload.tags"
+                  :key="tag.label"
+                  severity="success"
+                  :value="tag.value"></Tag>
+              </div>
+            </div>
+            <div class="flex flex-row">
+              <Button
+                @click="openAttachmentOverlay($event, fileDownload.files)"
+                :label="$t('attachment-with-number', fileDownload.files.length, { count: fileDownload.files.length }).capitalizeFirstLetter()"
+                icon="fa-paperclip"
+                size="small">
+              </Button>
+            </div>
+          </div>
+          <Divider />
+        </div>
+      </template>
+    </DataView>
+
+    <AttachmentOverlay ref="AttachmentOverlayRef" />
+  </Dialog>
+</template>
+
+<script setup lang="ts">
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+  import { storeToRefs } from 'pinia';
+  import DataView from 'primevue/dataview';
+  import Dialog from 'primevue/dialog';
+  import Divider from 'primevue/divider';
+  import Tag from 'primevue/tag';
+  import { computed, type Ref, ref } from 'vue';
+
+  import AttachmentOverlay from '@/components/partials/AttachmentOverlay.vue';
+  import type { FileDownload, MediaObject } from '@/models/ui-models';
+  import { useAttachmentOverlayStore } from '@/stores/attachmentOverlayStore';
+  import { useFileDownloadStore } from '@/stores/fileDownloadStore';
+
+  const fileDownloadStore = useFileDownloadStore();
+  const { fileDownloads, isFileDownloadDialogOpen } = storeToRefs(fileDownloadStore);
+
+  const attachmentOverlayStore = useAttachmentOverlayStore();
+
+  const AttachmentOverlayRef: Ref<typeof AttachmentOverlay | null> = ref(null);
+
+  const searchText = ref('');
+
+  const openAttachmentOverlay = (event: PointerEvent | MouseEvent, attachments: MediaObject[]) => {
+    if (AttachmentOverlayRef.value) {
+      attachmentOverlayStore.setCurrentViewedAttachments(attachments);
+      AttachmentOverlayRef.value.toggle(event);
+    }
+  };
+
+  const filteredDocuments = computed((): FileDownload[] => {
+    if (!searchText.value) return fileDownloads.value;
+
+    return fileDownloads.value.filter((file) => {
+      return (file.headline.toLowerCase().includes(searchText.value.toLowerCase()))
+        || (file.subHeadline.toLowerCase().includes(searchText.value.toLowerCase()))
+        || (file.description.toLowerCase().includes(searchText.value.toLowerCase()))
+        || (file.metaTags.join('').toLowerCase().includes(searchText.value.toLowerCase()));
+    });
+  });
+</script>
+
+<style>
+/*
+.p-tag {
+  background-color: rgb(224, 123, 45);
+  @apply text-white
+}
+*/
+
+.downloads-data-view-body {
+  .p-dataview-content {
+    max-height: 70vh;
+    overflow-y: auto;
+    @extend .custom-scrollbar
+  }
+}
+
+</style>
