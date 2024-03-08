f593643df395b57fbcf143c67b7d44b3aaec53a5
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2024-03-08 18:34:22 +0100

    Adjust app view for snap scroll

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,13 +2,17 @@
   <div
     class="relative"
     :key="selectedLanguage.strapiLocalCode">
-    <div v-if="shouldShouldHeaderBar">
+    <div
+      v-if="shouldShouldHeaderBar"
+      ref="HeaderBarRef">
       <HeaderBar />
     </div>
     <!--  Used to show global messages to  -->
     <Toast />
     <RouterView :key="selectedLanguage.strapiLocalCode" />
-    <FooterBar />
+    <div ref="FooterBarRef">
+      <FooterBar v-if="showFooterBar"/>
+    </div>
 
     <!--  Cookie Accept Dialog  -->
     <CookiesDialog />
@@ -21,7 +25,7 @@
 
     <Button
       v-if="showDownloadDialogButton"
-      class="h-12 w-12 flex flex-row justify-center fixed bottom-16 right-10"
+      class="hidden lg:flex flex-row fixed h-12 w-12 justify-center bottom-10 right-10"
       size="large"
       severity="success"
       @click="fileDownloadStore.toggleFileDownloadDialog">
@@ -38,7 +42,7 @@
   import Button from 'primevue/button';
   import Toast, { type ToastMessageOptions } from 'primevue/toast';
   import { useToast } from 'primevue/usetoast';
-  import { computed, onBeforeMount, onMounted, watch } from 'vue';
+  import { computed, onBeforeMount, onMounted, type Ref, ref, watch } from 'vue';
   import { RouterView, useRoute } from 'vue-router';
 
   import CookiesDialog from '@/components/partials/CookiesDialog.vue';
@@ -46,6 +50,7 @@
   import GlobalDownloadDialog from '@/components/partials/GlobalDownloadDialog.vue';
   import GuestDisclaimerDialog from '@/components/partials/GuestDisclaimerDialog.vue';
   import HeaderBar from '@/components/partials/HeaderBar.vue';
+  import { useApplicationStore } from '@/stores/applicationStore';
   import { useDataStore } from '@/stores/dataStore';
   import { useFeatureToggleStore } from '@/stores/featureToggleStore';
   import { useFileDownloadStore } from '@/stores/fileDownloadStore';
@@ -53,14 +58,19 @@
   import { useToastMessageStore } from '@/stores/toastMessageStore';
   import { useUserInfoStore } from '@/stores/userInfoStore';
 
-  const route = useRoute();
+  const HeaderBarRef: Ref<HTMLElement | undefined> = ref();
+  const FooterBarRef: Ref<HTMLElement | undefined> = ref();
 
+  const route = useRoute();
   const toast = useToast();
 
   const toastMessageStore = useToastMessageStore();
   const { toastMessages } = storeToRefs(toastMessageStore);
 
   const featureToggleStore = useFeatureToggleStore();
+
+  const applicationStore = useApplicationStore();
+  const { isMobile } = storeToRefs(applicationStore);
 
   const dataStore = useDataStore();
   const fileDownloadStore = useFileDownloadStore();
@@ -69,14 +79,20 @@
   const { selectedLanguage } = storeToRefs(languageStore);
 
   const userInfoStore = useUserInfoStore();
-  const { isUserLoggedIn } = storeToRefs(userInfoStore);
+  const { userSelectedSettings } = storeToRefs(userInfoStore);
 
   const shouldShouldHeaderBar = computed((): boolean => {
     return route.name !== 'login';
   });
 
   const showDownloadDialogButton = computed((): boolean => {
-    return route.meta.hasDownloadDialogButton;
+    return !!route.meta.hasDownloadDialogButton;
+  });
+
+  const showFooterBar = computed((): boolean => {
+    const isOnHomeView = route.name === 'home';
+
+    return !(isMobile.value && userSelectedSettings.value.useSnapScrollHomeView) || !isOnHomeView;
   });
 
   onBeforeMount(async (): Promise<void> => {
@@ -89,7 +105,22 @@
 
     // asynchronously fetch all data needed for the home view
     dataStore.fetchAllInformationForHomeView();
+
+    determineMaxBodyHeight();
   });
+
+  /**
+   * This method is used to get the information about the height of the header, footer and window to calculate the actual maximum
+   * view width the content on the user screen can have. The logic itself as well as the calculated height is stored in the pinia store
+   * todo: call this when the screen is resized
+   */
+  const determineMaxBodyHeight = (): void => {
+    const headerHeight = HeaderBarRef.value ? HeaderBarRef.value.clientHeight : 0;
+    const footerHeight = FooterBarRef.value ? FooterBarRef.value.clientHeight : 0;
+    const innerWindowHeight = window.innerHeight;
+
+    applicationStore.calculateMaxBodyHeight(headerHeight, footerHeight, innerWindowHeight);
+  };
 
   watch(toastMessages, async (newToastMessages: ToastMessageOptions[]) => {
     if (newToastMessages.length) {
